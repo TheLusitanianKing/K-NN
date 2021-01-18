3bf9442c413e28f7c36ea6a25cbf9d2743b66d14
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2021-01-18 19:33:31 +0000

    basic K-NN done (still have to weigh things better and find a better K)

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import Classifier
-import Liga (parseCSVFile)
+import LigaClassifier (nearestNeighbours, parseCSVFile)
 
 main :: IO ()
-main = parseCSVFile "data/liga-nos.csv" >>= mapM_ print
+main = do
+    clubs <- parseCSVFile "data/liga-nos.csv"
+    let benfica = head clubs
+    mapM_ print $ nearestNeighbours 5 benfica (tail clubs)
